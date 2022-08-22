c265cc22a192c85e8490e6d52d1e10cc5248f168
Teddy Pratama Riska <tedypr.109@gmail.com>
2022-08-22 14:02:11 +0700

    add package validator for chack email format, modify forgot and reset password

--- src/pages/examples/ForgotPassword.js
+++ src/pages/examples/ForgotPassword.js
@@ -4,10 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row, Form, Card, Button, Container, InputGroup, Modal } from '@themesberg/react-bootstrap';
 import { Link } from 'react-router-dom';
+import validator from "validator";
 import sendEmailIcon from "../../assets/img/illustrations/sendEmail.svg";
 import "./ForgotPassword.css"
 
 import { Routes } from "../../routes";
+import { getToken } from "../../function/helpers";
+import encryptData from "../../function/encryptData";
+import axios from "axios";
 
 
 export default () => {
@@ -15,22 +19,28 @@ export default () => {
   const [email, setEmail] = useState('')
   const [showModal, setShowModal] = useState(false)
 
-  function sendEmail(emailLink) {
-    if (emailLink.length > 0) {
-      let isEmail = 0
-      for (let i = 0; i < emailLink.length; i++) {
-        const email = emailLink[i];
-        if (email === "@") {
-          isEmail++
-        }
-      }
-      if (isEmail === 1) {
+  async function sendEmail(emailLink) {
+    try {
+      if (validator.isEmail(emailLink)) {
+        console.log('email valid');
+        const auth = "Bearer " + getToken()
+        const dataParams = encryptData(`{"email": "${emailLink}"}`)
+        const headers = {
+          "Content-Type": "application/json",
+          'Authorization': auth,
+        };
+        const emailSended = await axios.post("/Account/ForgotPassword", { data: dataParams }, { headers: headers })
+        console.log(emailSended, "ini email sended");
         setShowModal(true)
-      } else if (isEmail < 1 || isEmail > 1) {
+      } else {
+        console.log('email invalid');
         alert('Silahkan isi alamat email anda')
       }
-    } else {
-      alert('Silahkan isi alamat email anda')
+    } catch (error) {
+      // console.log(error)
+      if (error.response.status === 400) {
+        setShowModal(true)
+      }
     }
   }
 
