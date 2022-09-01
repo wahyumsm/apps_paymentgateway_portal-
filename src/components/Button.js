8bf2afb1b8dabc6f256f2be16854b4eb9f4ac7c4
Zalfa <zalva@ezeelink.co.id>
2022-09-01 08:49:27 +0700

    button atur waktu add payment

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,59 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChevronUp, faChevronDown} from "@fortawesome/free-solid-svg-icons";
 
-export class Button extends React.Component {
-    state = {
-      value: 12,
-      value2: 10
-    };
-  
-    onPlus= () => {
-      this.setState({
-        value: this.state.value +1
-      });
-          
-    };  
-      
-    onMinus = () => {
-      this.setState({
-        value: this.state.value - 1
-      });
-    };
-    
-    onPlusMin= () => {
-        this.setState({
-          value2: this.state.value2 +1
-        });
-            
-    };  
-        
-    onMinusMin = () => {
-        this.setState({
-          value2: this.state.value2 - 1
-        });
-    };
-  
-    render(){
-      return (
+function Button () {
+    const [value, setValue] = useState(20)
+    const [value2, setValue2] = useState(10)
+
+    const onPlus = () => {
+        if(value < 23) {
+            setValue(value+1)
+        } else {
+            setValue(0)
+        }
+    }
+
+    const onMinus = () => {
+        setValue(value-1)
+    }
+
+    const onPlusMin = () => {
+        setValue2(value2 + 1)
+    }
+
+    const onMinusMin = () => {
+        setValue2(value2 - 1)
+    }
+
+    return (
         <div className="d-flex justify-content-center align-items-center">
             <div className="counting d-flex flex-column align-items-center">
-                <button className="plus text-center" style={{border: "unset", background: "unset"}} onClick={this.onPlus}><FontAwesomeIcon icon={faChevronUp} color="#848484" /></button>
-                <div type="number" className="count text-center px-2 my-2" style={{background: "#F0F0F0", borderRadius: 4}} name="qty">{this.state.value}</div>
-                <button className="minus text-center" style={{border: "unset", background: "unset"}} onClick={this.onMinus}><FontAwesomeIcon icon={faChevronDown} color="#848484" /></button>          
+                <button className="plus text-center" style={{border: "unset", background: "unset"}} onClick={onPlus}><FontAwesomeIcon icon={faChevronUp} color="#848484" /></button>
+                <div type="number" className="count text-center px-2 my-2" style={{background: "#F0F0F0", borderRadius: 4}} name="qty">{value}</div>
+                <button className="minus text-center" style={{border: "unset", background: "unset"}} onClick={onMinus}><FontAwesomeIcon icon={faChevronDown} color="#848484" /></button>          
             </div>
             <div className="mx-3">:</div>
             <div className="counting d-flex flex-column align-items-center">
-                <button className="plus text-center" style={{border: "unset", background: "unset"}} onClick={this.onPlusMin}><FontAwesomeIcon icon={faChevronUp} color="#848484" /></button>
-                <div type="number" className="count text-center px-2 my-2" style={{background: "#F0F0F0", borderRadius: 4}} name="qty">{this.state.value2}</div>
-                <button className="minus text-center" style={{border: "unset", background: "unset"}} onClick={this.onMinusMin}><FontAwesomeIcon icon={faChevronDown} color="#848484" /></button>          
+                <button className="plus text-center" style={{border: "unset", background: "unset"}} onClick={onPlusMin}><FontAwesomeIcon icon={faChevronUp} color="#848484" /></button>
+                <div type="number" className="count text-center px-2 my-2" style={{background: "#F0F0F0", borderRadius: 4}} name="qty">{value2}</div>
+                <button className="minus text-center" style={{border: "unset", background: "unset"}} onClick={onMinusMin}><FontAwesomeIcon icon={faChevronDown} color="#848484" /></button>          
             </div>
         </div>
-      );
-    }
-  
-  
-  }
-  
-  
-  export default Button;
+    )
+}
+
+export default Button
