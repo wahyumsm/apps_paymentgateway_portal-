83cc739d7f74ede6241e94753a7497dc922baa99
Teddy Pratama Riska <tedypr.109@gmail.com>
2023-06-19 10:45:18 +0700

    modify in file helpers add new api baseUrl for test web

--- src/function/helpers.js
+++ src/function/helpers.js
@@ -2,6 +2,7 @@
 // export const BaseURL = "https://apid.ezeelink.co.id/mobile-demo/mobile2"
 // export const BaseURL = "https://apid.ezeelink.co.id/mobile-demo/mobile1"
 // export const BaseURL = "https://apid.ezeelink.co.id/snap/api2"
+// export const BaseURL = "https://apid.ezeelink.co.id/ezpaygateway/portal/"
 export const BaseURL = "https://api.ezeelink.co.id/ezpaygateway/portal"
 
 export const authorization = "Basic ZXplZWxpbms6ZXplZWxpbms=";
