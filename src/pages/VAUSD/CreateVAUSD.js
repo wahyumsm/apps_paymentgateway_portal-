493fc5d42d152b2797bcae7e2a86b18355532bb9
Teddy Pratama Riska <tedypr.109@gmail.com>
2023-09-21 09:35:49 +0700

    modify in file createVAUSD, and add 2 new icon

--- src/pages/VAUSD/CreateVAUSD.js
+++ src/pages/VAUSD/CreateVAUSD.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import breadcrumbsIcon from "../../assets/icon/breadcrumbs_icon.svg"
 import $ from 'jquery'
+import noteInfo from "../../assets/icon/note_icon_grey_transparent_bg.svg"
+import downloadIcon from "../../assets/icon/download_icon.svg"
+import refreshIcon from "../../assets/icon/refresh_icon.svg"
+import { Col, Row } from '@themesberg/react-bootstrap'
+import { convertToRupiah } from '../../function/helpers'
 
 function CreateVAUSD() {
 
@@ -53,7 +58,7 @@ function CreateVAUSD() {
                 <h2 className="h4 mt-4" style={{ fontFamily: "Exo", fontSize: 18, fontWeight: 700 }}>Virtual Account USD</h2>
             </div>
             <div className='base-content mt-3 pb-4'>
-                <div className='detail-akun-menu' style={{display: 'flex', height: 33}}>
+                <div className='detail-akun-menu' style={{fontFamily: "Exo", display: 'flex', height: 33}}>
                     <div className='detail-akun-tabs menu-detail-akun-hr-active' onClick={() => pindahHalaman("create")} id="createTab">
                         <span className='menu-detail-akun-span menu-detail-akun-span-active' id="createSpan">Buat VA Baru</span>
                     </div>
@@ -65,7 +70,85 @@ function CreateVAUSD() {
                     </div>
                 </div>
                 <hr className='hr-style mb-4' style={{marginTop: -2}}/>
-                <span className='font-weight-bold mt-3 mb-4' style={{fontWeight: 700}}>Buat VA</span>
+                <span className='font-weight-bold mt-3' style={{fontFamily: "Exo", fontWeight: 700}}>Buat VA</span>
+                <div className='d-flex justify-content-start align-items-center mt-3 mb-3' style={{ color: '#383838', padding: '14px 25px 14px 14px', fontSize: 14, fontStyle: 'italic', whiteSpace: 'normal', backgroundColor: 'rgba(255, 214, 0, 0.16)', borderRadius: 4 }}>
+                    <div className='ms-2'>Virtual Account akan aktif selama <b>30 hari</b> setelah dibuat, dan akan aktif sejak tanggal <b>26/08/2023 - 24/09/2023</b></div>
+                </div>
+                <div className='my-4'>
+                    <div style={{ fontSize: 14, fontWeight: 400, marginBottom: 10 }}>Jumlah data VA baru</div>
+                    <input
+                        type="number"
+                        className="input-text-ez"
+                        // onChange={handleChangeMaksTransaksi}
+                        value={0}
+                        name="dataVA"
+                        placeholder={"Rp 0"}
+                        style={{ width: "9%", marginLeft: "unset", }}
+                        // style={{ width: "100%", marginLeft: "unset", borderColor: alertMaksTransaksi ? "red" : "" }}
+                        // onBlur={() => setEditMaksTransaksi(!editMaksTransaksi)}
+                        min={0}
+                        max={1000}
+                        onKeyDown={(evt) => ["e", "E", "+", "-", ","].includes(evt.key) && evt.preventDefault()}
+                    />
+                    <button 
+                        // className={dataFromUpload.length === 0 ? 'btn-noez-transfer' : 'btn-ez-transfer'} //untukcsv
+                        className={'btn-ez-transfer'} //untuk excel
+                        style={{ width: '18%', marginLeft: 10 }}
+                    >
+                        Generate Virtual Account
+                    </button>
+                </div>
+                <hr className='hr-style mb-4' style={{marginTop: -2}}/>
+                <div className='my-4'>
+                    <div style={{ fontFamily: "Exo", fontSize: 16, fontWeight: 600 }}>Daftar File & VA</div>
+                    <Row>
+                        <Col>
+                            <Row className='d-flex justify-content-start'>
+                                <Col xs={3} className="card-information mt-3" style={{border: '1px solid #EBEBEB', height: 'fit-content', padding: '12px 0px 12px 16px'}}>
+                                    {/* <Row>
+                                        <Col xs={2} style={{ padding: 'unset', marginLeft: 5 }}>
+                                            <img src={noteInfo} width="15" height="15" alt="circle_info" />
+                                        </Col>
+                                        <Col xs={6} style={{ padding: 'unset' }}>
+                                            <p className="p-info" style={{ paddingLeft: 5 }}>Total sisa stok VA Tersedia</p>
+                                        </Col>
+                                        <Col xs={4} style={{ padding: 'unset' }}>
+                                            <p style={{ fontFamily: "Exo", fontSize: 25, fontWeight: 700 }}>10</p>
+                                        </Col>
+                                    </Row> */}
+                                    <div className='d-flex'>
+                                        <img src={noteInfo} width="20" height="20" alt="circle_info" />
+                                        <span className="p-info" style={{ paddingLeft: 7, width: 110 }}>Total sisa stok VA Tersedia: </span>
+                                        <span style={{ fontFamily: "Exo", fontSize: 25, fontWeight: 700, paddingRight: 10, marginTop: 5 }}>{convertToRupiah(1000, false)}</span>
+                                    </div>
+                                </Col>
+                                <Col xs={3} className="card-information mt-3 ms-3" style={{border: '1px solid #EBEBEB', height: 'fit-content', padding: '12px 0px 12px 16px'}}>
+                                    <div className='d-flex'>
+                                        <img src={noteInfo} width="20" height="20" alt="circle_info" />
+                                        <span className="p-info" style={{ paddingLeft: 7, width: 110 }}>Total VA Belum Tersedia: </span>
+                                        <span style={{ fontFamily: "Exo", fontSize: 25, fontWeight: 700, paddingRight: 10, marginTop: 5 }}>{convertToRupiah(1000, false)}</span>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col>
+                            <Row className='d-flex justify-content-end'>
+                                <Col xs={3} className="card-information mt-3" style={{border: '1px solid #077E86', height: 44, padding: '8px 24px'}}>
+                                    <div className='d-flex'>
+                                        <img src={downloadIcon} width="24" height="24" alt="download_icon" />
+                                        <span style={{ paddingLeft: 7, fontFamily: 'Exo', fontSize: 18, fontWeight: 700, color: '#077E86' }}>Download File CSV</span>
+                                    </div>
+                                </Col>
+                                <Col xs={3} className="card-information mt-3 ms-3" style={{border: '1px solid #077E86', height: 44, padding: '8px 24px'}}>
+                                    <div className='d-flex'>
+                                        <img src={refreshIcon} width="24" height="24" alt="refresh_icon" />
+                                        <span className="p-info" style={{ paddingLeft: 7, fontFamily: 'Exo', fontSize: 18, fontWeight: 700, color: '#077E86' }}>Perbarui Data</span>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         </div>
     )
