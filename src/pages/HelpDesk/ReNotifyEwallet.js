86f87bf593b5a7cf9f17434b6a5c89c241e9063d
Zalfa <zalva@ezeelink.co.id>
2023-10-17 14:12:45 +0700

    add API submit renotify and resend renotify

--- src/pages/HelpDesk/ReNotifyEwallet.js
+++ src/pages/HelpDesk/ReNotifyEwallet.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react'
 import breadcrumbsIcon from "../../assets/icon/breadcrumbs_icon.svg"
 import { Link, useHistory } from 'react-router-dom'
-import { Col, Form, Row } from '@themesberg/react-bootstrap'
+import { Button, Col, Form, Image, Modal, Row } from '@themesberg/react-bootstrap'
 import DataTable from 'react-data-table-component'
 import { BaseURL, convertDateTimeStamp, convertToRupiah, errorCatch, getToken, setUserSession } from '../../function/helpers'
 import encryptData from '../../function/encryptData'
 import axios from 'axios'
+import loadingEzeelink from "../../assets/img/technologies/Double Ring-1s-303px.svg"
+import JSONPretty from 'react-json-pretty'
 
 const ReNotifyEwallet = () => {
     const history = useHistory()
     const [noEwallet, setNoEwallet] = useState("")
     const [dataEwallet, setDataEwallet] = useState({})
-    const [dataHistoryNotify, setDataHistoryNotify] = useState({})
+    const [dataHistoryNotifyEwallet, setDataHistoryNotifyEwallet] = useState({})
+    const [getDetailNotificationEwallet, setGetDetailNotificationEwallet] = useState({})
+    const [showModalDataNotifyEwallet, setShowModalDataNotifyEwallet] = useState(false)
+    const [showModalSubmitEwallet, setShowModalSubmitEwallet] = useState(false)
+    var isHTML = RegExp.prototype.test.bind(/(<([^>]+)>)/i);
 
     function handleChange(e) {
         setNoEwallet(e.target.value)
@@ -29,19 +35,130 @@ const ReNotifyEwallet = () => {
             if (getDataEwallet.status === 200 && getDataEwallet.data.response_code === 200 && getDataEwallet.data.response_new_token === null) {
                 getDataEwallet.data.response_data.results.history_notify = getDataEwallet.data.response_data.results.history_notify.map((obj, idx) => ({...obj, number: idx + 1}))
                 setDataEwallet(getDataEwallet.data.response_data.results)
-                setDataHistoryNotify(getDataEwallet.data.response_data.results.history_notify)
+                setDataHistoryNotifyEwallet(getDataEwallet.data.response_data.results.history_notify)
                 // console.log(getDataEwallet.data.response_data.results.history_notify[0].request_data, 'response_data');
             } else if (getDataEwallet.status === 200 && getDataEwallet.data.response_code === 200 && getDataEwallet.data.response_new_token !== null) {
                 getDataEwallet.data.response_data.results.history_notify = getDataEwallet.data.response_data.results.history_notify.map((obj, idx) => ({...obj, number: idx + 1}))
                 setUserSession(getDataEwallet.data.response_new_token)
                 setDataEwallet(getDataEwallet.data.response_data.results)
-                setDataHistoryNotify(getDataEwallet.data.response_data.results.history_notify)
+                setDataHistoryNotifyEwallet(getDataEwallet.data.response_data.results.history_notify)
             }
         } catch (error) {
             // console.log(error);
             history.push(errorCatch(error.response.status))
         }
     }
+
+    async function submitReNotify(noEwallet) {
+        try {
+            const auth = 'Bearer ' + getToken();
+            const dataParams = encryptData(`{"emoney_code": "${noEwallet}"}`)
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': auth
+            }
+            const submittedReNotify = await axios.post(BaseURL + "/Helpdesk/SubmitReNotifyEmoney", { data: dataParams }, { headers: headers })
+            if (submittedReNotify.status === 200 && submittedReNotify.data.response_code === 200 && submittedReNotify.data.response_new_token === null) {
+                alert(submittedReNotify.data.response_data.results.Message)
+                setShowModalSubmitEwallet(false)
+                window.location.reload()
+            } else if (submittedReNotify.status === 200 && submittedReNotify.data.response_code === 200 && submittedReNotify.data.response_new_token !== null) {
+                setUserSession(submittedReNotify.data.response_new_token)
+                alert(submittedReNotify.data.response_data.results.Message)
+                setShowModalSubmitEwallet(false)
+                window.location.reload()
+            }
+        } catch (error) {
+            // console.log(error);
+            if (error.response.status === 400 && error.response.data.response_code === 400) {
+                alert(error.response.data.response_message)
+            }
+            history.push(errorCatch(error.response.status))
+        }
+    }
+
+    async function resendNotifyEwallet(noEwallet) {
+        try {
+            const auth = 'Bearer ' + getToken();
+            const dataParams = encryptData(`{"emoney_code": "${noEwallet}"}`)
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': auth
+            }
+            const resendedNotify = await axios.post(BaseURL + "/HelpDesk/ReSendNotificationEmoney", { data: dataParams }, { headers: headers })
+            if (resendedNotify.status === 200 && resendedNotify.data.response_code === 200 && resendedNotify.data.response_new_token === null) {
+                alert(resendedNotify.data.response_data.results.Message)
+                window.location.reload()
+            } else if (resendedNotify.status === 200 && resendedNotify.data.response_code === 200 && resendedNotify.data.response_new_token !== null) {
+                setUserSession(resendedNotify.data.response_new_token)
+                alert(resendedNotify.data.response_data.results.Message)
+                window.location.reload()
+            }
+        } catch (error) {
+            // console.log(error);
+            if (error.response.status === 400 && error.response.data.response_code === 400) {
+                alert(error.response.data.response_message)
+            }
+            history.push(errorCatch(error.response.status))
+        }
+    }
+
+    function getDetailNotifEwallet (id) {
+        setShowModalDataNotifyEwallet(true)
+        const findData = dataHistoryNotifyEwallet.find(item => item.number === id)
+        // console.log(findData, "findData");
+        setGetDetailNotificationEwallet(findData)
+    }
+
+    const columns = [
+        {
+            name: "Waktu",
+            selector: row => row.date,
+            width: "200px"
+        },
+        {
+            name: "URL",
+            selector: row => row.url,
+            wrap: true,
+            width: "340px"
+        },
+        {
+            name: "HTTP Status",
+            selector: row => row.response_code,
+            width: "150px"
+        },
+        {
+            name: 'Aksi',
+            width: "200px",
+            cell: (row) => (
+                <div className='text-center p-1' onClick={() => getDetailNotifEwallet(row.number)} style={{ cursor: "pointer", background: "linear-gradient(180deg, #F1D3AC 0%, #E5AE66 100%)", border: "0.6px solid #383838", borderRadius: 6, fontFamily: "Exo", color: "#2C1919", fontWeight: 700, fontSize: 14, width: 152 }}>
+                    Lihat Detail
+                </div>
+            )
+        },
+    ]
+
+    const customStyles = {
+        headCells: {
+            style: {
+                backgroundColor: '#F2F2F2',
+                border: '12px',
+                fontWeight: 'bold',
+                fontSize: '16px',
+                display: 'flex',
+                justifyContent: 'flex-start',
+                width: '150px'
+            },
+        },
+    };
+
+    const CustomLoader = () => (
+        <div style={{ padding: '24px' }}>
+          <Image className="loader-element animate__animated animate__jackInTheBox" src={loadingEzeelink} height={80} />
+          <div>Loading...</div>
+        </div>
+    );
+
     return (
         <div className="content-page mt-6">
             <span className='breadcrumbs-span'><Link to={"/"}>Beranda</Link>  &nbsp;<img alt="" src={breadcrumbsIcon} />  &nbsp;Re-Notify E-Wallet</span>
@@ -182,26 +299,26 @@ const ReNotifyEwallet = () => {
                                     Notes
                                 </span>
                             </Col>
-                            {/* {
-                                dataVirtualAccount.notes !== undefined &&
+                            {
+                                dataEwallet.notes !== undefined &&
                                 <Col xs={10}>
                                     <Form.Control
-                                        value={dataVirtualAccount.notes}
+                                        value={dataEwallet.notes}
                                         disabled
                                         type='text'
-                                        style={{ background: (dataVirtualAccount.is_warning) ? "#FDEAEA" : "rgba(7, 126, 134, 0.08)", color: "#077E86", height: 40, marginTop: '-7px', marginLeft: 'unset' }}
+                                        style={{ background: (dataEwallet.is_warning) ? "#FDEAEA" : "rgba(7, 126, 134, 0.08)", color: "#077E86", height: 40, marginTop: '-7px', marginLeft: 'unset' }}
                                         />
                                 </Col>
-                            } */}
+                            }
                         </Row>
                     </div>
 
-                    {/* <div className='base-content mt-4' style={{ width:"100%", padding: 50 }}>
+                    <div className='base-content mt-4' style={{ width:"100%", padding: 50 }}>
                         <div className="pb-2" style={{ fontFamily: "Exo", fontWeight: 700, fontSize: 16, color: "#383838" }}>Tabel Notifikasi</div>
                         <div className="div-table">
                             <DataTable
                                 columns={columns}
-                                data={dataHistoryNotify}
+                                data={dataHistoryNotifyEwallet}
                                 customStyles={customStyles}
                                 // pagination
                                 highlightOnHover
@@ -212,24 +329,78 @@ const ReNotifyEwallet = () => {
 
                     <div style={{ display: "flex", justifyContent: "end", marginRight: -15, width: "unset", padding: "0px 15px" }}>
                         <button
-                            onClick={() => resendNotify(noVA)}
-                            className={(dataVirtualAccount.is_success === undefined || dataVirtualAccount.is_success === false) ? "btn-off mt-3 mb-3 me-2" : 'add-button mt-3 mb-3 me-2'}
+                            onClick={() => resendNotifyEwallet(noEwallet)}
+                            className={(dataEwallet.is_success === undefined || dataEwallet.is_success === false) ? "btn-off mt-3 mb-3 me-2" : 'add-button mt-3 mb-3 me-2'}
                             style={{ maxWidth: 'max-content', padding: 7, height: 40, }}
-                            disabled={dataVirtualAccount.is_success === undefined || dataVirtualAccount.is_success === false}
+                            disabled={dataEwallet.is_success === undefined || dataEwallet.is_success === false}
                         >
                             Resend Notification
                         </button>
                         <button
-                            onClick={() => setShowModalSubmit(true)}
-                            className={(dataVirtualAccount.is_success === undefined || dataVirtualAccount.is_success !== false) ? "btn-off mt-3 mb-3" : 'add-button mt-3 mb-3'}
+                            onClick={() => setShowModalSubmitEwallet(true)}
+                            className={(dataEwallet.is_success === undefined || dataEwallet.is_success !== false) ? "btn-off mt-3 mb-3" : 'add-button mt-3 mb-3'}
                             style={{ maxWidth: 'max-content', padding: 7, height: 40, }}
-                            disabled={dataVirtualAccount.is_success === undefined || dataVirtualAccount.is_success !== false}
+                            disabled={dataEwallet.is_success === undefined || dataEwallet.is_success !== false}
                         >
                             Submit Re-Notify
                         </button>
-                    </div> */}
+                    </div>
                 </div>
             </div>
+
+            <Modal
+                size="lg"
+                centered
+                show={showModalSubmitEwallet}
+                onHide={() => setShowModalSubmitEwallet(false)}
+                style={{ display: "flex", borderRadius: 8, justifyContent: "center" }}
+            >
+                <Modal.Body style={{  width: "100%", padding: "12px 24px" }}>
+                    <div style={{ display: "flex", justifyContent: "center", marginTop: 32, marginBottom: 16 }}>
+                        <p style={{ fontFamily: "Exo", fontSize: 20, fontWeight: 700, marginBottom: "unset" }} className="text-center">Apakah Kamu yakin ingin menyimpan perubahan ini?</p>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "center", marginTop: 32, marginBottom: 16 }}>
+                        <p style={{ fontFamily: "Nunito", fontSize: 14, fontWeight: 400, marginBottom: "unset" }} className="text-center">Pastikan transaksi tersebut telah berhasil.</p>
+                    </div>
+                    <p>
+                        
+                    </p>                
+                    <div className="d-flex justify-content-center mb-3">
+                        <Button onClick={() => setShowModalSubmitEwallet(false)} style={{ fontFamily: "Exo", color: "#888888", background: "#FFFFFF", maxWidth: 125, maxHeight: 45, width: "100%", height: "100%", border: "1px solid #EBEBEB;", borderColor: "#EBEBEB" }} className="mx-2">Tidak</Button>
+                        <Button onClick={() => submitReNotify(noEwallet)} style={{ fontFamily: "Exo", color: "black", background: "linear-gradient(180deg, #F1D3AC 0%, #E5AE66 100%)", maxWidth: 125, maxHeight: 45, width: "100%", height: "100%" }}>Ya</Button>
+                    </div>
+                </Modal.Body>
+            </Modal>
+
+            <Modal
+                size="lg"
+                centered
+                show={showModalDataNotifyEwallet}
+                onHide={() => setShowModalDataNotifyEwallet(false)}
+                style={{ display: "flex", borderRadius: 8, justifyContent: "center" }}
+            >
+                <Modal.Body style={{  width: "100%", padding: "12px 24px" }}>
+                    <div style={{ fontFamily: "Exo", fontSize: 20, fontWeight: 700, marginBottom: "unset" }}>
+                        <div className='pb-3'>Request Data</div>
+                        <div className='p-3' style={{ fontFamily: "Nunito", fontSize: 16, border: "1px solid #EBEBEB", borderRadius: 6 }}><JSONPretty id="json-pretty" data={getDetailNotificationEwallet.request_data}></JSONPretty></div>
+                    </div>
+                    <hr />
+                    <div style={{ fontFamily: "Exo", fontSize: 20, fontWeight: 700, marginBottom: "unset" }}>
+                        <div className='pb-3'>Response Data</div>
+                        {
+                            isHTML(getDetailNotificationEwallet.response_data) !== true ? (
+                                <div className='p-3' style={{ fontFamily: "Nunito", fontSize: 16, border: "1px solid #EBEBEB", borderRadius: 6 }}><JSONPretty id="json-pretty" data={getDetailNotificationEwallet.response_data}></JSONPretty></div>
+                            ) : (
+                                <div style={{ fontFamily: "Nunito", fontSize: 16 }} dangerouslySetInnerHTML={{ __html: getDetailNotificationEwallet.response_data }} />
+                            )
+                        }
+                    </div>   
+                    <hr />           
+                    <div className="d-flex justify-content-center my-3">
+                        <Button onClick={() => setShowModalDataNotifyEwallet(false)} style={{ fontFamily: "Exo", color: "#888888", background: "#FFFFFF", maxWidth: 125, maxHeight: 45, width: "100%", height: "100%", border: "1px solid #EBEBEB;", borderColor: "#EBEBEB" }} className="mx-2">OKE</Button>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
