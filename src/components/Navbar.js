1fd1fa08daa4f83deb5f957a898ddbc414a39871
Teddy Pratama Riska <tedypr.109@gmail.com>
2023-11-21 09:09:58 +0700

    modify in file htaccess, package.json, index.js & navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -565,10 +565,10 @@ export default (props) => {
                 // (window.location.href === 'https://localhost:3000/ezeepg#/ezeepg/alokasisaldo') ||
                 // (window.location.href === 'https://localhost:3000/ezeepg#/ezeepg/riwayat-transaksi/disbursement')
 
-                // (window.location.href === 'http://reactdev/dev3/#/dev3/disbursement') ||
-                // (window.location.href === 'http://reactdev/dev3/#/dev3/riwayattopup') ||
-                // (window.location.href === 'http://reactdev/dev3/#/dev3/alokasisaldo') ||
-                // (window.location.href === 'http://reactdev/dev3/#/dev3/riwayat-transaksi/disbursement')
+                || (window.location.href === 'http://reactdev/dev2/#/dev2/disbursement') ||
+                (window.location.href === 'http://reactdev/dev2/#/dev2/riwayattopup') ||
+                (window.location.href === 'http://reactdev/dev2/#/dev2/alokasisaldo') ||
+                (window.location.href === 'http://reactdev/dev2/#/dev2/riwayat-transaksi/disbursement')
 
                 // (window.location.href === 'https://localhost:3000/dev3#/dev3/disbursement') ||
                 // (window.location.href === 'https://localhost:3000/dev3#/dev3/riwayattopup') ||
