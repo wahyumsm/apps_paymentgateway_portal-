d61832c1c43019292b6c484467fc9f16b3cfa5fa
Zalfa <zalva@ezeelink.co.id>
2024-01-04 14:30:09 +0700

    page daftar merchant qris

--- src/pages/DaftarMerchantQris/DaftarMerchantQris.js
+++ src/pages/DaftarMerchantQris/DaftarMerchantQris.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import breadcrumbsIcon from "../../assets/icon/breadcrumbs_icon.svg";
 import $ from 'jquery'
+import DataTable from 'react-data-table-component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Image } from '@themesberg/react-bootstrap';
+import { agenLists } from '../../data/tables';
+import FilterComponent from '../../components/FilterComponent';
+import loadingEzeelink from "../../assets/img/technologies/Double Ring-1s-303px.svg"
 
 const DaftarMerchantQris = () => {
     function disbursementTabs(isTabs){
@@ -27,6 +34,83 @@ const DaftarMerchantQris = () => {
             $('#merchantOutletspan').addClass('menu-detail-akun-span-active')
         }
     }
+
+    const [pending, setPending] = useState(true)
+    const [filterText, setFilterText] = React.useState('');
+    const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
+    const filteredItems = agenLists.filter(
+        item => item.namaAgen && item.namaAgen.toLowerCase().includes(filterText.toLowerCase()),
+    );
+
+    const subHeaderComponentMemo = useMemo(() => {
+        const handleClear = () => {
+            if (filterText) {
+                setResetPaginationToggle(!resetPaginationToggle);
+                setFilterText('');
+            }
+        };
+        return (
+            <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} title="Cari Daftar Partner :" placeholder="Masukkan Nama Partner" />
+        );	}, [filterText, resetPaginationToggle]
+    );
+
+    const columns = [
+        {
+            name: 'No',
+            selector: row => row.id,
+            width: '67px'
+        },
+        {
+            name: 'ID Partner',
+            selector: row => row.IDAgen,
+            width: "130px"
+        },
+        {
+            name: 'Nama Perusahaan',
+            selector: row => row.namaAgen,
+            wrap: true,
+            sortable: true,
+            width: "230px"
+        },
+        {
+            name: 'Email Perusahaan',
+            selector: row => row.email,
+            wrap: true,
+            sortable: true
+        },
+        {
+            name: 'No. Telepon',
+            selector: row => row.noHp,
+            sortable: true
+        },
+        {
+            name: 'Status',
+            selector: row => row.status,
+            width: "180px",
+            sortable: true
+        },
+    ];
+
+    const customStyles = {
+        headCells: {
+            style: {
+                backgroundColor: '#F2F2F2',
+                border: '12px',
+                fontWeight: 'bold',
+                fontSize: '16px',
+                paddingRight: 'none'
+                
+            },
+        },
+    };
+
+    const CustomLoader = () => (
+        <div style={{ padding: '24px' }}>
+          <Image className="loader-element animate__animated animate__jackInTheBox" src={loadingEzeelink} height={80} />
+          <div>Loading...</div>
+        </div>
+    );
+
     return (
         <div className="main-content mt-5" style={{padding: "37px 27px 37px 27px"}}>
             <span className='breadcrumbs-span'><span style={{ cursor: "pointer" }}>Beranda</span> &nbsp;<img alt="" src={breadcrumbsIcon} /> &nbsp;<span style={{ cursor: "pointer" }}>Daftar Merchant</span></span>
@@ -37,16 +121,35 @@ const DaftarMerchantQris = () => {
                 <div className='detail-akun-tabs menu-detail-akun-hr-active' id="merchantGrup" onClick={() => disbursementTabs("merchantGrup")}>
                     <span className='menu-detail-akun-span menu-detail-akun-span-active' id="merchantGrupspan">Merchant grup</span>
                 </div>
-                <div className='detail-akun-tabs' style={{marginLeft: 15}} id="merchantBrand">
+                <div className='detail-akun-tabs' style={{marginLeft: 15}} id="merchantBrand" onClick={() => disbursementTabs("merchantBrand")}>
                     <span className='menu-detail-akun-span' id="merchantBrandspan">Merchant brand</span>
                 </div>
-                <div className='detail-akun-tabs' style={{marginLeft: 15}} id="merchantOutlet">
+                <div className='detail-akun-tabs' style={{marginLeft: 15}} id="merchantOutlet" onClick={() => disbursementTabs("merchantOutlet")}>
                     <span className='menu-detail-akun-span' id="merchantOutletspan">Merchant outlet</span>
                 </div>
             </div>
             <hr className='hr-style' style={{marginTop: -2}}/>
-            <div className='base-content'>
-
+            <div className='base-content positiion-relative'>
+                <div style={{ display: "flex", justifyContent: "end", marginTop: -10, paddingBottom: 24 }}>
+                    <button style={{ fontFamily: "Exo", fontSize: 16, fontWeight: 700, alignItems: "center", padding: "12px 24px", gap: 8, width: 201, height: 48, background: "linear-gradient(180deg, #F1D3AC 0%, #E5AE66 100%)", border: "0.6px solid #2C1919", borderRadius: 6 }}>
+                        <FontAwesomeIcon icon={faPlus} style={{ marginRight: 10 }} /> Tambah Partner
+                    </button>
+                </div>
+                <div className="div-table">
+                    <DataTable
+                        columns={columns}
+                        data={filteredItems}
+                        customStyles={customStyles}
+                        pagination
+                        highlightOnHover
+                        // progressPending={pending}
+                        paginationResetDefaultPage={resetPaginationToggle}
+                        progressComponent={<CustomLoader />}
+                        subHeader
+                        subHeaderComponent={subHeaderComponentMemo}
+                        persistTableHead
+                    />
+                </div>
             </div>
         </div>
     )
